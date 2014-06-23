df50a73eaaa80c5ec1c9bb1bee3588c57e730f5e
Gonçalo Martins <gondsm@gmail.com>
2014-06-23 15:39:53 +0100

    Translated solution 7.

--- 007/7.cpp
+++ 007/7.cpp
@@ -1,8 +1,7 @@
-//Problema: encontrar o primo numero 10001
 #include <iostream>
 using namespace std;
 
-bool verifica(int n){
+bool verify(int n){
 	if(n == 2) return true;
 	if(n == 0 || n == 1) return false;
 	for(int i = 2; i < n; i++){
@@ -14,9 +13,9 @@ bool verifica(int n){
 int main(){
 	int count = 0;
 	for(int i = 0; 1; i++){
-		if(verifica(i)) count++;
+		if(verify(i)) count++;
 		if(count == 10001){
-			cout << "Primo 10001: " << i << '\n'; 
+			cout << "Prime 10001: " << i << '\n'; 
 			break;
 		}
 	}
