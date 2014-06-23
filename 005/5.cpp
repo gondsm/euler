b9a242731590928648002a9e6c7e3dff186d28b8
Gonçalo Martins <gondsm@gmail.com>
2014-06-23 15:37:42 +0100

    Translated and optimized solution 5.

--- 005/5.cpp
+++ 005/5.cpp
@@ -1,8 +1,6 @@
-//Problema: qual é o número mais pequeno que é divisível por todos os números de 1 a 20?
-//O algoritmo ainda precisa de melhoramentos, pois demorou mais que 1 minuto a resolver o problema. Cerca de 4-5min.
 #include <iostream>
 
-bool verificaSeDivisivel(int n){
+bool isDivisible(int n){
 	for(int i = 20; i > 2; i--)
 		if(n % i != 0) return false;
 
@@ -12,13 +10,12 @@ bool verificaSeDivisivel(int n){
 int main(){
 	int result = 0;
 	for(unsigned long long int i = 20; 1; i = i + 20){
-		if(verificaSeDivisivel(i)){
+		if(isDivisible(i)){
 			result = i;
 			break;
 		}
-		std::cout << i << "\n";
 	}
-	std::cout << "O menor numero divisivel por todos os numeros de 1 a 20 e " << result << std::endl;
+	std::cout << "The smallest number divisible by every integer between 1 and 20 is " << result << std::endl;
 
 	return 0;
 }
