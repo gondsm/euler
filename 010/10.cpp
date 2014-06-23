538903890c5c466046b081cbac09488bba12a019
Gonçalo Martins <gondsm@gmail.com>
2014-06-23 15:47:09 +0100

    Translated solution 10.

--- 010/10.cpp
+++ 010/10.cpp
@@ -1,26 +1,15 @@
-//Precisa de um algoritmo novo. O programa resolve teoricamente o problema,
-//mas demoraria várias horas.
-//Tenho dúvidas quanto à capacidade de um unsigned long long para guardar um
-//número tão grande.
-
-//v2.0: Utilização do teorema de Wilson para determinação dos números primos reduz
-//radicalmente o número de divisões (de n/2 para 1). Problema: factoriais de
-//números astronómicos.
-
-//v3: reescrita completa, utilizando o Crivo de Erastótenes.
-
-//Resolvido.
+//v3: Total rewrite, using the Sieve of Erastotenes.
 #include <iostream>
 #include <iomanip>
 #include <fstream>
 using namespace std;
 
 int main(){
-	//Criação da tabela:
+	// Table Creation:
 	unsigned int tab [2000000];
 	for(int i = 0; i < 2000000; i++)
 		tab[i] = i;
-	//Eliminação:
+	// Elimination:
 	int j = 2;
 	int p = 0;
 	while(p*p < 2000000){
@@ -29,7 +18,6 @@ int main(){
 		tab[p*p] = 0;
 		int q = p*p;
 		while(q < 2000000){
-			// cout << setw(7) << q << ' ' << p << endl;
 			tab[q] = 0;
 			q = q + p;
 		}
@@ -38,21 +26,12 @@ int main(){
 			j++;
 		}
 	}
-	//Soma:
-	
+	// Sum:
 	unsigned long long int soma = 0;
 	for(int i = 2; i < 2000000; i++){
 		soma += tab[i];
 	}
-	cout << "Soma = " << soma << endl;
-	
-	//Debug:
-	/*ofstream dados;
-	dados.open("tab.txt");
-	for(int i = 0; 1 < 2000000; i++){
-		if(tab[i] != 0) dados << tab[i] << ", ";
-	}
-	dados.close();*/
-			
+	cout << "Sum = " << soma << endl;
+
 	return 0;
 }
