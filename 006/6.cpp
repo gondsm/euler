2d6e9c4e5678823fb42b3799220b7893f4e620ff
Gonçalo Martins <gondsm@gmail.com>
2014-06-23 15:38:38 +0100

    Translated solution 6.

--- 006/6.cpp
+++ 006/6.cpp
@@ -1,20 +1,18 @@
-//Problema: Qual é a diferença entre a soma dos quadrados e o quadrado da soma dos primeiros 100 numeros naturais (1..100)?
-//O algoritmo resolveu o problema numa questão de milissegundos.
 #include <iostream>
 
 
 int main(){
-	//Soma dos quadrados:
+	// Sum of the squares:
 	int sum1 = 0;
 	for(int i = 1; i <= 100; i++)
 		sum1 = sum1 + (i*i);
-	//Quadrado da soma:
+	// Square of the sum:
 	int sum2 = 0;
 	for(int i = 1; i <=100; i++)
 		sum2 = sum2 + i;
 	sum2 = sum2 * sum2;
-	//Resultado
+	// Result
 	int result = sum2 - sum1;
-	std::cout << "A diferenca e " << result << "\n";
+	std::cout << "The difference is " << result << "\n";
 	return 0;
 }
